8d054b31bb0d372454af4c107f8514df26c967e8
Arekuti Nagi Reddy <nagi2038@gmail.com>
2024-09-03 10:42:21 +0530

    started spring project

---
+++ src/main/java/com/product/productcatelog/controllers/MyfirstController.java
@@ -0,0 +1,4 @@
+package com.product.productcatelog.controllers;
+
+public class MyfirstController {
+}
---
+++ src/main/java/com/product/productcatelog/services/MyFirstService.java
@@ -0,0 +1,4 @@
+package com.product.productcatelog.services;
+
+public class MyFirstService {
+}
